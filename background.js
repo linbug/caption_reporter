77b0e4d8c925502f68093467ee7580b4d2130561
Lin Taylor <taylorxlin@gmail.com>
2016-03-15 18:15:59 +0000

    cleans up comments

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 // Called when the url of a tab changes.
 function checkForValidUrl(tabId, changeInfo, tab) {
-// If the tabs url starts with "http://specificsite.com"...
+// If the tabs url starts with "https://www.youtube.com/watch?" (i.e. is a YouTube video page)
 if (tab.url.indexOf('https://www.youtube.com/watch?') == 0) {
 // ... show the page action.
 chrome.pageAction.show(tabId);
--- popup.js
+++ popup.js
@@ -1,12 +1,14 @@
 
+//wait until the popup is loaded
 document.addEventListener('DOMContentLoaded', function(){
 
-
+    //if the FCC button is pressed, open the FCC complaints tab
     document.getElementById("buttonfcc").addEventListener("click", function(){
         console.log('lol');
         chrome.tabs.create({ "url":"https://consumercomplaints.fcc.gov/hc/en-us/requests/new?ticket_form_id=36040" });
     });
 
+    //if the YouTube tab is pressed, open the YouTube complaints tab
     document.getElementById("buttonyou").addEventListener("click", function(){
         console.log('lol');
         var youtab = chrome.tabs.create({ "url":"https://www.youtube.com/caption_notification" });
