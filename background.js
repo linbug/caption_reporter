372ea8c20318bf8bd2bfef054d5662bdbc600066
Lin Taylor <taylorxlin@gmail.com>
2016-03-15 12:58:42 +0000

    only works for youtube video pages

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 // Called when the url of a tab changes.
 function checkForValidUrl(tabId, changeInfo, tab) {
 // If the tabs url starts with "http://specificsite.com"...
-if (tab.url.indexOf('https://www.youtube.com/') == 0) {
+if (tab.url.indexOf('https://www.youtube.com/watch?') == 0) {
 // ... show the page action.
 chrome.pageAction.show(tabId);
 }
